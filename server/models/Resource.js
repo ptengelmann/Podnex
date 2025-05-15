2e6df7d288bc4f2b7656139f89a21c2ddd0cd330
Pedro Serapiao <166184305+ptengelmann@users.noreply.github.com>
2025-05-15 11:23:59 +0100

    Create both Resources models and routes

---
+++ server/models/Resource.js
@@ -0,0 +1,46 @@
+// models/Resource.js
+const mongoose = require('mongoose');
+
+const ResourceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  fileUrl: {
+    type: String,
+    required: true
+  },
+  fileType: {
+    type: String,
+    required: true
+  },
+  fileSize: {
+    type: Number,
+    required: true
+  },
+  pod: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Pod',
+    required: true
+  },
+  uploadedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  tags: [{
+    type: String
+  }],
+  // For access control - if null, visible to all pod members
+  visibleTo: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Resource', ResourceSchema);
